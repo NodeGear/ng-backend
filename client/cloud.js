ff0b029898f9bbf4d2311a3428af87e21b7a9d02
Matej Kramny <matejkramny@gmail.com>
2013-08-13 08:29:08 +0100

    Create user model, experiment with CLI options in CLI binary

--- client/cloud.js
+++ client/cloud.js
@@ -3,22 +3,43 @@ var optimist = require('optimist')
 	, request = require('request')
 	, fs = require('fs')
 
-var tmp = '/tmp/'+Date.now()+'.tar.gz'
-console.log(tmp)
-exec('cd '+process.cwd()+' && tar czf '+tmp+' .', function(err) {
-	if (err) throw err;
+var argv = optimist
+	.usage('Use nodecloud -deploy', {
+		'deploy': {
+			description: 'Deploy current directory to nodecloud',
+			required: true,
+			alias: 'd'
+		},
+		'register': {
+			description: 'Register with nodecloud'
+		},
+		'login': {
+			description: 'Login with nodecloud'
+		}
+	})
+	.argv
+
+optimist.showHelp()
+
+if (argv.deploy) {
+	console.log("Deploying".magenta);
+	var tmp = '/tmp/'+Date.now()+'.tar.gz'
+	console.log(tmp)
+	exec('cd '+process.cwd()+' && tar czf '+tmp+' .', function(err) {
+		if (err) throw err;
 	
-	var r = request.post('http://nodecloud.matej.me/drone/create', function(err, res, body) {
-		console.log(body)
+		var r = request.post('http://nodecloud.matej.me/drone/create', function(err, res, body) {
+			console.log(body)
 		
-		exec('rm '+tmp, function(err) {
-			if (err){
-				console.log("Fail removing temp file");
-				throw err;
-			}
-		})
-	});
+			exec('rm '+tmp, function(err) {
+				if (err){
+					console.log("Fail removing temp file");
+					throw err;
+				}
+			})
+		});
 	
-	var form = r.form();
-	form.append('drone', fs.createReadStream(tmp));
-});	
+		var form = r.form();
+		form.append('drone', fs.createReadStream(tmp));
+	});
+}
