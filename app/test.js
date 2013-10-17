45cef6f69e02497b9f2e7be2154c607c881138b4
Matej Kramny <matejkramny@gmail.com>
2013-10-17 19:11:58 +0100

    Upgraded post hooks

--- app/test.js
+++ app/test.js
@@ -7,9 +7,6 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -31,4 +28,3 @@ app.get('/image.gif', function(req, res) {
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
-
--- lib/scripts/create_git_drone.sh
+++ lib/scripts/create_git_drone.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#cd $DRONE_LOCATION;
-#git reset --hard;
-#git pull /home/nodecloud/repositories/${GL_REPO}.git
-
 git clone /home/nodecloud/repositories/${GL_REPO}.git ${DRONE_LOCATION}
