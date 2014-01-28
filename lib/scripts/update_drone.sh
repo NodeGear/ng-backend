5e640d64135daa2c8532eed6caa07b417b5cf48b
Matej Kramny <matejkramny@gmail.com>
2014-01-28 21:07:57 +0000

    [WIP] Git round of fixes

--- lib/scripts/update_drone.sh
+++ lib/scripts/update_drone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd $DRONE_LOCATION;
+cd $1;
 git reset --hard &> /dev/null;
 # TODO security issue right here...
-git pull /home/git/repositories/${GL_REPO}.git &> /dev/null
+git pull /home/git/repositories/${2}.git &> /dev/null
 
 /usr/local/bin/npm install
