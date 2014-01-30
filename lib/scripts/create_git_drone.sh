2fdb589bb91c46c56b6f348ff16db9fb37168291
Matej Kramny <matejkramny@gmail.com>
2014-01-30 11:22:27 +0000

    Ability to create git apps hopefully

--- lib/scripts/create_git_drone.sh
+++ lib/scripts/create_git_drone.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# $1 Location
+# $2 git location
+# $3 user id
+
 git clone /home/git/repositories/${2}.git ${1}
+chown -R ${3}:${3} ${1}
+chmod 770 -R ${1}
 cd $1
 
 /usr/local/bin/npm install
