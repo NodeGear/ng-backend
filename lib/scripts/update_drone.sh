c1566c80cd3f18f398084c03855201aaea6d7ea3
Matej Kramny <matejkramny@gmail.com>
2014-01-30 15:47:38 +0000

    Fix updating drones

--- lib/scripts/update_drone.sh
+++ lib/scripts/update_drone.sh
@@ -3,6 +3,6 @@
 cd $1;
 git reset --hard;
 # TODO security issue right here...
-git pull /home/git/repositories/${2}.git &> /dev/null
+git pull /home/git/repositories/${2}.git master
 
 /usr/local/bin/npm install
