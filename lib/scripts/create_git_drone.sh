e5fac1ac2e4d8928a65e17490c7092a6de5e1804
Matej Kramny <matejkramny@gmail.com>
2014-01-30 19:22:24 +0000

    Refactor Drone to use normalised fields instead of pkg property

--- lib/scripts/create_git_drone.sh
+++ lib/scripts/create_git_drone.sh
@@ -3,10 +3,12 @@
 # $1 Location
 # $2 git location
 # $3 user id
+# $4 user $HOME
 
+mkdir -p ${4}/logs
 git clone /home/git/repositories/${2}.git ${1}
-chown -R ${3}:${3} ${1}
-chmod -R 770 ${1}
-cd $1
+chown -R ${3}:${3} ${4}
+chmod -R 770 ${4}
 
+cd $1
 /usr/local/bin/npm install
