684b53ccd404ce948bad1fca6acb87d3d238e8b6
Matej Kramny <matejkramny@gmail.com>
2014-03-12 19:05:31 +0000

    Improve template app creation

--- lib/scripts/create_template_drone.sh
+++ lib/scripts/create_template_drone.sh
@@ -4,6 +4,7 @@
 # $1 Location
 # $2 user id
 # $3 user $HOME
+# $4 Template location
 
 # Exit codes:
 # 0 - success
@@ -15,8 +16,7 @@
 
 mkdir -p ${3}/logs
 
-#/var/ng_templates/ghost/
-if [ -d "/Users/matejkramny/ghost/" ]; then
+if [ -d "${4}" ]; then
 	echo "Ghost Template exists.."
 else
 	echo "Ghost Template does Not Exist"
@@ -30,7 +30,7 @@ if [ -d "$1" ]; then
 else
 	echo "Copying template"
 	mkdir -p ${1}
-	cp -r /Users/matejkramny/ghost/ ${1}
+	cp -r ${4} ${1}
 fi
 
 # fixing permissions
